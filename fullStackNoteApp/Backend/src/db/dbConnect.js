8c3b65639f54064700a66cfead8b672837a59fb5
Karan Kumar <152979583+CodeByKaran@users.noreply.github.com>
2024-04-18 17:20:06 +0530

    Update dbConnect.js

--- fullStackNoteApp/Backend/src/db/dbConnect.js
+++ fullStackNoteApp/Backend/src/db/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 const dbConnect=async()=>{
     try{
-        const instance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`)
+        const instance = await mongoose.connect(`mongodb+srv://karan:kkaknk007@cluster0.lhrgtj6.mongodb.net/NoteApp`)
         console.log(`Db connected Succcesfully ${instance.connection.host}`);
     }catch(err){
         console.log("Error:",err);
